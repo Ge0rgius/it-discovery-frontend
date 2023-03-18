45630d8c0812e7d8af57b1e8a7f0e058aced61cd
Sergiy <sergey.morenets@gmail.com>
2023-03-18 15:56:35 +0200

    Task 6

---
+++ src/main.ts
@@ -0,0 +1,96 @@
+'use strict';
+//import {getInputValue, updateHTML} from "./utils.js";
+
+class Book {
+
+    title: any;
+
+    author: any;
+
+    publisher: any;
+
+    pages: number;
+
+    year: number;
+
+    constructor(title: string, author: string, publisher: string, pages: number, year: number) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.pages = pages;
+        this.year = year;
+    }
+
+}
+
+class BookStorage {
+    //static #instance;
+    static instance;
+
+    books = [];
+
+    static {
+        this.instance = new BookStorage();
+    }
+
+    static getInstance() {
+        return this.instance;
+    }
+
+    addBook(title: string, author: string, publisher: string, pages: number, year: number): void {
+        const book = new Book(title, author, publisher, pages, year);
+        this.books.push(book);
+    }
+
+    displayBooks(): void {
+        let text = '';
+        for(let book of this.books) {
+            text += `title:${book.title}, author: ${book.author}`;
+        }
+        updateHTML('bookList', text);
+    }
+}
+
+//TODO for homework
+class Author {
+
+}
+
+class Publisher {
+
+}
+
+class User {
+
+}
+
+class Order {
+
+}
+
+document.getElementById('addBook').addEventListener('click', () => {
+    const title = getInputValue('inputTitle') as string;
+    const author = <string>getInputValue('inputAuthor');
+    const publisher = getInputValue('inputPublisher') as string;
+    //TODO what if not a number?
+    const pages = Number(getInputValue('inputPages'));
+    const year = Number(getInputValue('inputYear'));
+
+    BookStorage.getInstance().addBook(title, author, publisher, pages, year);
+    BookStorage.getInstance().displayBooks();
+});
+
+function getInputValue(elementId: string): string {
+
+    const inputElement = <HTMLInputElement>document.getElementById(elementId);
+    return inputElement.value;
+}
+
+ function updateHTML(elementId: string, value: string): void {
+
+    const inputElement: HTMLElement = document.getElementById(elementId);
+    inputElement.innerHTML = value;
+}
+
+
+
