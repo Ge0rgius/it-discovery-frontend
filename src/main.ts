ded4179887094fe20f870a72addfb77a3eb5d778
Sergiy <sergey.morenets@gmail.com>
2023-03-18 18:29:42 +0200

    Task 9

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 'use strict';
+
 //import {getInputValue, updateHTML} from "./utils.js";
 
 class Book {
@@ -10,15 +11,46 @@ class Book {
         return `title:${this.title}, author: ${this.author}`;
     }
 
+    public match(title?: string, author?: string): boolean {
+        //TODO implement
+        return true;
+    }
+
 }
 
-class BookStorage {
-    static #instance: BookStorage;
+interface BookStore {
+    getBooks(): Book[];
+
+    addBook(book: Book): void;
+
+    search(title?: string, author?: string): Book[];
+}
+
+class InMemoryBookStore implements BookStore {
 
     private readonly books: Book[] = [];
 
+    addBook(book: Book): void {
+        this.books.push(book);
+    }
+
+    getBooks(): Book[] {
+        return this.books;
+    }
+
+    search(title?: string, author?: string): Book[] {
+        return this.books.filter(book => book.match(title, author));
+    }
+
+}
+
+class BookController {
+    static #instance: BookController;
+
+    private readonly bookStore: BookStore = new InMemoryBookStore();
+
     static {
-        this.#instance = new BookStorage();
+        this.#instance = new BookController();
     }
 
     public static getInstance() {
@@ -27,21 +59,30 @@ class BookStorage {
 
     public addBook(title: string, author: string, publisher: string, pages: number, year: number): void {
         const book = new Book(title, author, publisher, pages, year);
-        this.books.push(book);
+        this.bookStore.addBook(book);
     }
 
     public displayBooks(): void {
         let text = '';
-        for(let book of this.books) {
+        for (let book of this.bookStore.getBooks()) {
             text += book.getGeneralInformation();
         }
         updateHTML('bookList', text);
     }
 }
 
+abstract class Human {
+
+    constructor(private name: string, private birthDate: Date) {
+    }
+}
 
 //TODO for homework
-class Author {
+class Author extends Human {
+    constructor(private books: Book[], name: string, birthDate: Date) {
+        super(name, birthDate);
+    }
+
 
 }
 
@@ -49,7 +90,11 @@ class Publisher {
 
 }
 
-class User {
+class User extends Human {
+
+    constructor(private orders: Order[], name: string, birthDate: Date) {
+        super(name, birthDate);
+    }
 
 }
 
@@ -65,8 +110,8 @@ document.getElementById('addBook')?.addEventListener('click', () => {
     const pages = Number(getInputValue('inputPages'));
     const year = Number(getInputValue('inputYear'));
 
-    BookStorage.getInstance().addBook(title, author, publisher, pages, year);
-    BookStorage.getInstance().displayBooks();
+    BookController.getInstance().addBook(title, author, publisher, pages, year);
+    BookController.getInstance().displayBooks();
 });
 
 function getInputValue(elementId: string): string {
@@ -75,10 +120,10 @@ function getInputValue(elementId: string): string {
     return inputElement.value;
 }
 
- function updateHTML(elementId: string, value: string): void {
+function updateHTML(elementId: string, value: string): void {
 
     const inputElement: HTMLElement | null = document.getElementById(elementId);
-    if(inputElement) {
+    if (inputElement) {
         inputElement.innerHTML = value;
     }
 }
